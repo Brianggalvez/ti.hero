ce84c0da3044d35cc7ae35a9beca38459c3af995
brian garcía <bgarcia@itinerarium.cat>
2019-01-11 17:54:31 +0100

    updated example

--- example/app.js
+++ example/app.js
@@ -1,10 +1,33 @@
-var Hero = require('ti.hero');
-
 var win = Ti.UI.createWindow({
-	backgroundColor: 'white'
-});
-var label = Ti.UI.createLabel({
-	text: 'click me'
-});
-win.add(label);
-win.open();
+  backgroundColor: 'white'
+  // hero: { enabled: true }
+})
+var label = Ti.UI.createLabel()
+var view = Ti.UI.createView({
+  hero: { id: 'redView' },
+  height: 50,
+  width: 50,
+  backgroundColor: 'red'
+})
+win.add(label)
+win.add(view)
+win.open()
+
+var modal = Ti.UI.createWindow({
+  backgroundColor: 'white',
+  hero: { enabled: true },
+  modal: true
+})
+var viewModal = Ti.UI.createView({
+  hero: { id: 'redView' },
+  height: 50,
+  width: 50,
+  top: 50,
+  backgroundColor: 'red'
+})
+
+modal.add(viewModal)
+view.addEventListener('click', () => {
+  modal.open({ modal: true })
+  setTimeout(modal.close, 2000)
+})
